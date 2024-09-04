9c3cc53e418fda2fe290b97d05e3adb4bd3f04a2
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2024-09-04 22:05:42 +0500

    Commit added

--- ormsqlmodel/models/todo.py
+++ ormsqlmodel/models/todo.py
@@ -1,7 +1,6 @@
 
 from sqlmodel import SQLModel,  Field  # type: ignore
 
-#  Students model
 class Todo(SQLModel, table=True):  # type: ignore
     id: int = Field(default=None, primary_key=True)
     title: str
