4d3abe236ae12053beffd37d47d602d119c2c222
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2024-09-07 20:02:53 +0500

    updated

--- ormsqlmodel/config/db.py
+++ ormsqlmodel/config/db.py
@@ -2,14 +2,11 @@ from sqlmodel import SQLModel, create_engine, Session  # type: ignore
 import os
 from typing import Any
 
-# Load the connection string from environment variables
 connection_string: Any = os.getenv('DB_SECRET_STRING')
 
 print(connection_string)
 
-# Create the database connection
 connection = create_engine(connection_string)
 
 def create_tables():
-    # Create the tables in the database
     SQLModel.metadata.create_all(connection)
